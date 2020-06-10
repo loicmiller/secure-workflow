9d6f07d44ba0f5ddc7ad1610387e4251146104dc
Loïc Miller <loic.miller@etu.unistra.fr>
2020-06-10 15:51:03 +0200

    Debugging

--- movie-workflow/measurements/gke-measures.py
+++ movie-workflow/measurements/gke-measures.py
@@ -67,7 +67,7 @@ def get_pods(context):
         print(stdout)
 
     pods = []
-    for line in stdout[1:]:
+    for line in stdout.split('\n')[1:]:
         print(line)
         #pod_id = line.split()[0]
         #pod_name = pod_id.split('-')[0]
